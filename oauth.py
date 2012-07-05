51cd905d59063d229d211e08415264d24daee25d
Stephen Tran <stephen.v.tran@gmail.com>
2012-07-05 10:30:30 -0700

    Added request for access token with pre-verified request token

--- oauth.py
+++ oauth.py
@@ -1,5 +1,6 @@
 import oauth2 as oauth
 import urlparse
+import pprint # For debug
 
 consumer_key = 'key'
 consumer_secret = 'secret'
@@ -12,38 +13,48 @@ consumer = oauth.Consumer(consumer_key, consumer_secret)
 
 client = oauth.Client(consumer)
 
+# Get the request token
 resp, content = client.request(request_token_url, "GET")
 if resp['status'] != '200':
     raise Exception("Invalid response %s." % resp['status'])
 
 
 print resp
+print
 print content
-#request_token = dict(urlparse.parse_qsl(content))
-#
-#print "Request Token:"
-#print "    - oauth_token        = %s" % request_token['oauth_token']
-#print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
-#print
-#
-#print "Go to the following link in your browser:"
-#print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
-#print 
-#
-#token = oauth.Token(request_token['oauth_token'],
-#    request_token['oauth_token_secret'])
+print
+
+request_token = dict(urlparse.parse_qsl(content))
+
+
+#pprint.pprint(request_token)
+
+
+print "Request Token:"
+print "    - oauth_token        = %s" % request_token['oauth_token']
+print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
+print
+
+print "Go to the following link in your browser:"
+print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
+print 
+
+
+token = oauth.Token(request_token['oauth_token'],
+    request_token['oauth_token_secret'])
 #token.set_verifier(oauth_verifier)
-#client = oauth.Client(consumer, token)
-#
-#resp, content = client.request(access_token_url, "POST")
-#access_token = dict(urlparse.parse_qsl(content))
-#
-#print "Access Token:"
-#print "    - oauth_token        = %s" % access_token['oauth_token']
-#print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
-#print
-#print "You may now access protected resources using the access tokens above." 
-#print
+client = oauth.Client(consumer, token)
+
+# Get the access token
+resp, content = client.request(access_token_url, "POST")
+access_token = dict(urlparse.parse_qsl(content))
+
+print "Access Token:"
+print "    - oauth_token        = %s" % access_token['oauth_token']
+print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
+print
+print "You may now access protected resources using the access tokens above." 
+print
 
 
 
